ed39f211431c3d4b3bec88c4a46da8b9e94e637c
le7-3609 <3609le7@gmail.com>
2025-11-11 23:34:19 +0200

    Make the layer-model

--- WebApiShop/Controllers/UsersController.cs
+++ WebApiShop/Controllers/UsersController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
-using WebApiShop.Controllers;
+//using WebApiShop.Controllers;
+using Entities;
+using Repositories;
+using Services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,66 +14,50 @@ namespace WebApiShope.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        string _filePath = "E:\\WEB API\\WebApiShop\\WebApiShop\\usersFile.txt";
-
+        UsersService _usersService = new UsersService();
         // GET: api/<UsersController>
-        [HttpGet]
-        public List<Users> Get()
-        {
-            List<Users> allUsers = new();
-            using (StreamReader reader = System.IO.File.OpenText(_filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    Users user = JsonSerializer.Deserialize<Users>(currentUserInFile);
-                    allUsers.Add(user);
-                }
-            }
-            return allUsers;
-        }
+        //[HttpGet]
+        //public List<Users> Get()
+        //{
+        //    List<Users> allUsers = new();
+        //    using (StreamReader reader = System.IO.File.OpenText(_filePath))
+        //    {
+        //        string? currentUserInFile;
+        //        while ((currentUserInFile = reader.ReadLine()) != null)
+        //        {
+        //            Users user = JsonSerializer.Deserialize<Users>(currentUserInFile);
+        //            allUsers.Add(user);
+        //        }
+        //    }
+        //    return allUsers;
+        //}
 
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
         public ActionResult<Users> Get(int id)
         {
-            using (StreamReader reader = System.IO.File.OpenText(_filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    Users user = JsonSerializer.Deserialize<Users>(currentUserInFile);
-                    if (user.UserId == id)
-                        return Ok(user);
-                }
+            Users user = _usersService.UsersServiceGetById(id);
+            if(user == null) {
+                return NoContent();
             }
-            return NoContent(); //write in c# code
+            return Ok(user);
         }
 
         // POST api/<UsersController>
         [HttpPost]
         public ActionResult<Users> Post([FromBody] Users user)
         {
-            int numberOfUsers = System.IO.File.ReadLines(_filePath).Count();
-            user.UserId = numberOfUsers + 1;
-            string userJson = JsonSerializer.Serialize(user);
-            System.IO.File.AppendAllText(_filePath, userJson + Environment.NewLine);
-            return CreatedAtAction(nameof(Get), new { id = user.UserId }, user);
+            Users newUser = _usersService.UsersServicePost(user);
+            return CreatedAtAction(nameof(Get), new { id = user.UserId }, newUser);
         }
 
         [HttpPost("login")]
         public ActionResult<Users> Login([FromBody] ExistUser oldUser)
         {
-            using (StreamReader reader = System.IO.File.OpenText(_filePath))
-            {
-                string? currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    Users user = JsonSerializer.Deserialize<Users>(currentUserInFile);
-                    if (user.Email == oldUser.Email && user.Password == oldUser.Password)
-                        return CreatedAtAction(nameof(Get), new { user.UserId }, user);
-                }
+            Users user = _usersService.UsersServiceLogin(oldUser);
+            if(user != null) {
+                return CreatedAtAction(nameof(Get), new { id = user.UserId }, user);
             }
             return NoContent();
         }
@@ -79,30 +66,14 @@ namespace WebApiShope.Controllers
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] Users userToUpdate)
         {
-            string textToReplace = string.Empty;
-            using (StreamReader reader = System.IO.File.OpenText(_filePath))
-            {
-                string currentUserInFile;
-                while ((currentUserInFile = reader.ReadLine()) != null)
-                {
-                    Users user = JsonSerializer.Deserialize<Users>(currentUserInFile);
-                    if (user.UserId == id)
-                        textToReplace = currentUserInFile;
-                }
-            }
-
-            if (textToReplace != string.Empty)
-            {
-                string text = System.IO.File.ReadAllText(_filePath);
-                text = text.Replace(textToReplace, JsonSerializer.Serialize(userToUpdate));
-                System.IO.File.WriteAllText(_filePath, text);
-            }
+            _usersService.UsersServicePut(id, userToUpdate);
         }
 
         // DELETE api/<UsersController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _usersService.UsersServiceDelete(id);
         }
     }
 }
