48eaf56fafe9260558b13feb28175f719db08d39
le7-3609 <240203746+le7-3609@users.noreply.github.com>
2025-11-15 19:32:02 +0200

    Fix .gitignore and clear tracked build folders

---
+++ Repositories/IUsersRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+
+namespace Repositories
+{
+    public interface IUsersRepository
+    {
+        void Delete(int id);
+        Users GetById(int id);
+        Users Login(ExistUser oldUser);
+        Users Post(Users user);
+        void Put(int id, Users userToUpdate);
+    }
+}
